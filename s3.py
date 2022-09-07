4224be8b0786b469d1494d3c908ddf7eecdf5f0c
thebrnwal <61709835+thebrnwal@users.noreply.github.com>
2022-09-07 12:51:30 +0530

    Add files via upload

--- s3.py
+++ s3.py
@@ -1,23 +1,23 @@
-import boto3
+import boto3                                                                                        
+import sys                                                                                                                                                                                                    
 
+#bucket_name = input("Enter Bucket Name : ")                                                            
 
-bucket_name = input(Enter Bucket Name :)
+client = boto3.clinet('s3')                                                                                                                                                                                    
 
-client = boto3.client('s3')
-response = client.get_bucket_acl(Bucket=bucket_name)
-print = (response)
-
-if "'Permission': 'FULL_CONTROL" in str(response):
-	print("Full control")
-
-elif "'Permission': 'READ'}" in str(response):
-	print("Read Access")
-
-elif "'Permission': 'WRITE'}" in str(response):
-	print("WRITE Access")
-
-elif "'Permission': 'READ_ACP'}" in str(response):
-	print("READ_ACP")
-
-elif "'Permission': 'WRITE_ACP'}" in str(response):
-	print("WRITE_ACP")
+with open(sys.argv[1], 'r') as file:                                                                      
+    for bucket_name in file:                                                                                
+        response = client.get_bucket_acl(Bucket=bucket_name)                                                    
+        print("start testing {}").format(bucket_name)                                                        
+        if "'Permission': 'FULL_CONTROL" in str(response):                          
+            print ("Full control")                                                  
+        elif "'Permission': 'READ'}" in str(response):      
+            print ("Read Access")          
+        elif "'Permission': 'WRITE'}" in str(response):  
+            print ("WRITE Access")          
+        elif "'Permission': 'READ_ACP'}" in str(response):          
+            print ("READ_ACP")        
+        elif "'Permission': 'WRITE_ACP'}" in str(response):          
+            print ("WRITE_ACP")                          
+        else:                                
+            print("bucket not found")
